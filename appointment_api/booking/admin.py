df54365779cc198f1899b98e5b2236149f17091e
Nicole Nereah <nicole@learning.git>
2025-12-14 17:14:55 +0300

    Registered my models in the admin.py

--- appointment_api/booking/admin.py
+++ appointment_api/booking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import ServiceType , Appointment, AppointmentSlot, Feedback
 # Register your models here.
+
+admin.site.register(ServiceType)
+admin.site.register(Appointment)
+admin.site.register(AppointmentSlot)
+admin.site.register(Feedback)
