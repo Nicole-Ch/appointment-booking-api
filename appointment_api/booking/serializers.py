1b0999843b518c9c8b72797b69705a2c1cd94f4d
Nicole Nereah <nicole@learning.git>
2025-12-14 18:52:10 +0300

    Created Serializers for my models

---
+++ appointment_api/booking/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from .models import ServiceType, CustomUser, AppointmentSlot, Appointment, Feedback
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'is_provider']
+
+
+class ServiceTypeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ServiceType
+        fields = "__all__"
+
+
+class AppointmentSlotSerializer(serializers.ModelSerializer):
+    provider = CustomUserSerializer(read_only = True)
+
+
+    class Meta:
+        model = AppointmentSlot
+        fields = ['id', 'provider', 'service_type', 'start_time', 'end_time', 'is_booked']
+
+class AppointmentSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer(read_only = True)
+    slot = AppointmentSlotSerializer(read_only = True)
+
+
+    class Meta:
+        model = Appointment
+        fields = ['id', 'user', 'slot', 'status', 'created_at', 'notes']
+
+class FeedbackAppointment(serializers.ModelSerializer):
+   
+    user = CustomUser(read_only = True)
+
+    class Meta:
+        model = Feedback
+        fields = ['appointment' , 'user', 'rating', 'comment', 'created_at']
+
