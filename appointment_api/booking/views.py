1d1492213437959aef4d65a579d0138156efd200
Nicole Nereah <nicole@learning.git>
2025-12-18 16:50:10 +0300

    Created Service and Appointmentslot api views

--- appointment_api/booking/views.py
+++ appointment_api/booking/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from .models import Feedback, CustomUser,Appointment,AppointmentSlot,  ServiceType
+from rest_framework import generics
+from .serializers import AppointmentSerializer,AppointmentSlotSerializer,CustomUserSerializer,FeedbackSerializer, ServiceTypeSerializer
 # Create your views here.
+
+
+class ServiceTypeList(generics.ListAPIView):
+    queryset = ServiceType.objects.all()
+    serializer_class = ServiceTypeSerializer
+
+class ServiceTypeRetrieve(generics.RetrieveAPIView):
+    queryset = ServiceType.objects.all()
+    serializer_class = ServiceTypeSerializer
+
+class AppointmentSlotList(generics.ListAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSlotSerializer
+
+class AppointmentSlotRetrieve(generics.RetrieveAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSlotSerializer            
