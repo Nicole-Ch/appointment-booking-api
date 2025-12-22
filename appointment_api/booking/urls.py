3ebc8a3b224546252faa043532d8f2fb56c35920
Nicole Nereah <nicole@learning.git>
2025-12-22 16:56:47 +0300

    Created Urls for slotand appointment create

--- appointment_api/booking/urls.py
+++ appointment_api/booking/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path , include
-from .views import AppointmentSlotRetrieve,AppointmentSlotList,ServiceTypeList,ServiceTypeRetrieve
+from .views import AppointmentSlotRetrieve,AppointmentSlotList,ServiceTypeList,ServiceTypeRetrieve,AppointmentCreateView,SlotCreateView
 
 
 urlpatterns = [
    path('service/', ServiceTypeList.as_view(), name='services' ),
    path('service/<int:pk>/', ServiceTypeRetrieve.as_view, name='serviceDetail'),
    path('slot/', AppointmentSlotList.as_view(), name='slots'),
-   path('slot/<int:pk>', AppointmentSlotRetrieve.as_view(), name='slotDetail')
+   path('slot/<int:pk>', AppointmentSlotRetrieve.as_view(), name='slotDetail'),
+
+   path('slot/create/' , SlotCreateView.as_view(), name='slot-create'),
+   path('appointment/create', AppointmentCreateView.as_view(), name='appointment-create')
 
 ]
 
