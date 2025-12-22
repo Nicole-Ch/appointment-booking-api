aa73bc60d0c3ce616289ea2eed2203be9466eed4
Nicole Nereah <nicole@learning.git>
2025-12-22 08:53:07 +0300

    Added SlotCreateView for slot creation

--- appointment_api/booking/views.py
+++ appointment_api/booking/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Feedback, CustomUser,Appointment,AppointmentSlot,  ServiceType
 from rest_framework import generics ,  permissions
-from .serializers import AppointmentSerializer,AppointmentSlotSerializer,CustomUserSerializer,FeedbackSerializer, ServiceTypeSerializer
+from .serializers import AppointmentSerializer,AppointmentSlotSerializer,CustomUserSerializer,FeedbackSerializer, ServiceTypeSerializer,AppointmentCreateSerializer
+from . permissions import IsProvider
 # Create your views here.
 
 
@@ -23,4 +24,17 @@ class AppointmentSlotList(generics.ListAPIView):
 class AppointmentSlotRetrieve(generics.RetrieveAPIView):
     queryset = Appointment.objects.select_related("provider", "service_type").all()
     serializer_class = AppointmentSlotSerializer 
-    permission_classes = [permissions.AllowAny]           
+    permission_classes = [permissions.AllowAny] 
+
+
+# Appointment Slot-Creation  
+class SlotCreateView(generics.CreateAPIView):
+    serializer_class = AppointmentCreateSerializer
+    permission_classes = [permissions.IsAuthenticated,IsProvider]
+
+    def perform_create(self, serializer):
+        # ensure provider cannot create slots for another user
+        provider = self.request.user
+         # save the new slot and set its provider
+        serializer.save(provider=provider)
+
