835de58c05989f561de5e33a95704c3a7f80d751
Nicole Nereah <nicole@learning.git>
2025-12-23 15:47:20 +0300

    Added Comments to urls

--- appointment_api/booking/urls.py
+++ appointment_api/booking/urls.py
@@ -4,14 +4,14 @@ from .views import AppointmentListView, AppointmentSlotRetrieve,AppointmentSlotL
 
 
 urlpatterns = [
-   path('service/', ServiceTypeList.as_view(), name='services' ),
-   path('service/<int:pk>/', ServiceTypeRetrieve.as_view, name='serviceDetail'),
-   path('slot/', AppointmentSlotList.as_view(), name='slots'),
-   path('slot/<int:pk>', AppointmentSlotRetrieve.as_view(), name='slotDetail'),
+   path('service/', ServiceTypeList.as_view(), name='services' ), #View available services
+   path('service/<int:pk>/', ServiceTypeRetrieve.as_view, name='serviceDetail'),# view service detail
+   path('slot/', AppointmentSlotList.as_view(), name='slots'), #view appointment slots
+   path('slot/<int:pk>', AppointmentSlotRetrieve.as_view(), name='slotDetail'), #view slot details
 
-   path('slot/create/' , SlotCreateView.as_view(), name='slot-create'),
-   path('appointment/create', AppointmentCreateView.as_view(), name='appointment-create'),
-   path("appointments/", AppointmentListView.as_view(), name="appointment-list"),
+   path('slot/create/' , SlotCreateView.as_view(), name='slot-create'), #provider creates a slot
+   path('appointment/create', AppointmentCreateView.as_view(), name='appointment-create'), #customer creates appointment
+   path("appointments/", AppointmentListView.as_view(), name="appointment-list"), #list of ll appointments
 
 ]
 
