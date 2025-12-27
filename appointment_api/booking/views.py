a4725eefc3a61a7b19e46391bbeb069af2d7f242
Nicole Nereah <nicole@learning.git>
2025-12-27 10:22:20 +0300

    Created a view for Login users

--- appointment_api/booking/views.py
+++ appointment_api/booking/views.py
@@ -18,15 +18,38 @@ class RegisterView(APIView):
     def post(self,request):
         serializer = RegisterSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.save()
+        user = serializer.save() #returns a user instance
 
         token, _ = Token.objects.get_or_create(user=user)
         return Response({
             "id":user.pk,
             'username': user.username,
             'email': user.email,
-            'token': Token.key
+            'token': token.key
         }, status=status.HTTP_201_CREATED)
+    
+
+class LoginView(APIView):
+     permission_classes = [permissions.AllowAny]
+
+     def post(self,request):
+         serializer = LoginSerializer(data=request.data)
+         serializer.is_valid(raise_exception=True)
+         username = serializer.validated_data['username']
+         password = serializer.validated_data['password']
+
+         user = authenticate(request, username=username, password=password)
+
+         if user is None:
+             return Response({'status':'User not found'}, status=status.HTTP_401_UNAUTHORIZED)
+         
+         token , created = Token.objects.get_or_create(user=user)
+         return Response({'token': token.key,
+                           'username': username,
+                           'password':password
+                          
+                          })
+
 
 
 class ServiceTypeList(generics.ListAPIView):
