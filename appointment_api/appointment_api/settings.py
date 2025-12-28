71d0533a730b6edb344e51022a0fe82f24b293ad
Nicole Nereah <nicole@learning.git>
2025-12-28 11:42:17 +0300

    Preparing for deployment

--- appointment_api/appointment_api/settings.py
+++ appointment_api/appointment_api/settings.py
@@ -102,12 +102,11 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'mysql://root:TNYBold$2025@localhost:3306/django_appointment'),
-        conn_max_age=600
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
